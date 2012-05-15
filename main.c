ee0129de419bd05d2ef9884c416ea8ae3560c096
Tamas Kovacs <kovtom@kovnet.hu>
2012-05-15 21:46:14 +0200

    EEPROM-ból byte beolvasás és mentés
    Signed-off-by: Tamas Kovacs <kovtom@kovnet.hu>

--- main.c
+++ main.c
@@ -6,14 +6,42 @@
  */
 
 #include "lcd.h"
+#include <stdlib.h>
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/eeprom.h>
+
+#define SPACE_LINE "                        "
+uint8_t EEMEM counter_ee;
 
 int main(void) {
+	char buffer[4];
 
 	lcd_init(LCD_DISP_ON);		//LCD init
 	lcd_clrscr();				//LCD clear and cursor home
 	lcd_puts("Ezt irjuk ki");	//LCD-re string kiirasa
 
-	for (;;);					//Loop...
+	_delay_ms(2000);
+
+	uint8_t counter;
+	counter = eeprom_read_byte(&counter_ee);
+
+	for (;;) {
+		if(!(counter % 10)) {
+			lcd_puts("Oszthato tizzel...");
+			_delay_ms(2000);
+			lcd_gotoxy(0,1);
+			lcd_puts(SPACE_LINE);
+			eeprom_update_byte(&counter_ee, counter);
+		}
+		itoa(counter, buffer, 10);
+		lcd_gotoxy(0,1);
+		lcd_puts(buffer);
+		_delay_ms(1000);
+		++counter;
+
+
+
+	}
 	return 0;
 }
